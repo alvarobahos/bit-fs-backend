5d9a2b0912e834eaccf40441d6f54e65defbfc82
alvarobahos <alvarobahosc@gmail.com>
2025-02-28 21:27:35 -0500

    advance

--- bit-fs/backend/src/controladores/productos.js
+++ bit-fs/backend/src/controladores/productos.js
@@ -1,24 +1,74 @@
+import ProductosModel from "../modelos/productos.js";
+
 class ProductosController {
   constructor() {}
 
   async crear(solicitud, respuesta) {
-    respuesta.json({ mensaje: "crear,listo", data: null });
+    try {
+      const resultado = await ProductosModel.create(solicitud.body);
+      respuesta.json({ mensaje: "se creó un nuevo producto", data: resultado });
+    } catch (error) {
+      respuesta.json({
+        mensaje: "ocurrió un error al crear producto",
+        data: error,
+      });
+    }
   }
 
   async leerTodos(solicitud, respuesta) {
-    respuesta.json({ mensaje: "leerTodos,listo", data: null });
+    try {
+      const resultado = await ProductosModel.getAll();
+      respuesta.json({
+        mensaje: "se obtuvieron todos los productos",
+        data: resultado,
+      });
+    } catch (error) {
+      respuesta.json({
+        mensaje: "ocurrió un error al obtener todos los pructos",
+        data: error,
+      });
+    }
   }
 
   async leerUno(solicitud, respuesta) {
-    respuesta.json({ mensaje: "leerUno,listo", data: null });
+    try {
+      const resultado = await ProductosModel.getOne(solicitud.params.id);
+      respuesta.json({ mensaje: "se obtuvo el producto", data: resultado });
+    } catch (error) {
+      respuesta.json({
+        mensaje: "no se encontró el producto con el id: " + solicitud.params.id,
+        data: error,
+      });
+    }
   }
 
   async actualizar(solicitud, respuesta) {
-    respuesta.json({ mensaje: "actualizar,listo", data: null });
+    try {
+      console.log("id:", solicitud.params.id);
+      console.log("body:", solicitud.body);
+      const resultado = await ProductosModel.update(
+        solicitud.params.id,
+        solicitud.body
+      );
+      respuesta.json({ mensaje: "producto actualizado", data: resultado });
+    } catch (error) {
+      respuesta.json({
+        mensaje: "ocurrió un error al actualizar el producto",
+        data: error,
+      });
+    }
   }
 
   async eliminar(solicitud, respuesta) {
-    respuesta.json({ mensaje: "eliminar,listo", data: null });
+    try {
+      const resultado = await ProductosModel.delete(solicitud.params.id);
+      respuesta.json({ mensaje: "producto eliminado", data: resultado });
+    } catch (error) {
+      respuesta.json({
+        mensaje: "ocurrió un error al eliminar un producto",
+        data: error,
+      });
+    }
   }
 }
 export default new ProductosController();
